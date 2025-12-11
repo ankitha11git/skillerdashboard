a9f697bcf1770b7f46da5bcd472351ba98d10fd3
ankitha11git <usharanithigala@gmail.com>
2025-12-11 20:00:09 +0530

    added matplotlib

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create the plot
+plt.plot(x, y, marker='o', color='blue', linestyle='--')
+
+# Add labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Plot using Matplotlib")
+
+# Show the plot
+plt.show()
