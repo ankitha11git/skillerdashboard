ea92610dde76c5f9a9a7669f5fcaf91840ea685d
ankitha11git <usharanithigala@gmail.com>
2025-12-16 20:22:40 +0530

    commit from pycharm app

---
+++ Downloads/skillerdashboard-main/skillerdashboard-main/main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create the plot
+plt.plot(x, y, marker='o', color='blue', linestyle='--')
+
+# Add labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Plot using Matplotlib")
+
+# Show the plot
+plt.show()
+
+print("I did a project")
